190e016f4c7a6040ef6eb1d97a3beb55268cb131
Vania Elizondo <vaniaelizondo@V-2.local>
2019-08-07 19:26:04 -0500

    Updates

---
+++ precache-manifest.3406de5fc7a2bb392530ddc2340685a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a046d26d45ff08f0d75b9c537116e83a",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "731c14d3992eebb835bf",
+    "url": "/CV/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/CV/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "731c14d3992eebb835bf",
+    "url": "/CV/static/js/main.83a052e6.chunk.js"
+  },
+  {
+    "revision": "21043815baf811eb5681",
+    "url": "/CV/static/js/runtime~main.50de0404.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CV/static/media/logo.5d5d9eef.svg"
+  }
+]);
